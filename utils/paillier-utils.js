8ff1b1e971cf2655136135a41878ef855965a644
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-12-04 20:29:17 +0530

    minor: message tweaks

--- utils/paillier-utils.js
+++ utils/paillier-utils.js
@@ -8,18 +8,18 @@ async function paillierTest(m1, m2) {
 
     console.log(
         "Public key parameters: \n",
-        "n - ",
+        "n = ",
         publicKey.n.toString(),
         "\n",
-        "g - ",
+        "g = ",
         publicKey.g.toString()
     );
     console.log(
         "Private key parameters: \n",
-        "lambda - ",
+        "lambda = ",
         privateKey.lambda.toString(),
         "\n",
-        "mu - ",
+        "mu = ",
         privateKey.mu.toString()
     );
 
@@ -34,22 +34,17 @@ async function paillierTest(m1, m2) {
     const c2 = publicKey.encrypt(m2);
     const encryptedSum = publicKey.addition(c1, c2);
 
-    console.log("m1 - ", m1.toString());
-    console.log("m2 - ", m2.toString());
+    console.log("m1 = ", m1.toString());
+    console.log("m2 = ", m2.toString());
 
-    console.log("c1 - ", c1.toString());
-    console.log("c2 - ", c2.toString());
+    console.log("c1 = ", c1.toString());
+    console.log("c2 = ", c2.toString());
 
-    console.log("Encrypted sum - ", encryptedSum.toString());
+    console.log("Encrypted sum = ", encryptedSum.toString());
 }
 
 const args = process.argv.slice(2);
-const m1 = BigInt(args[0]);
-const m2 = BigInt(args[1]);
-
-if (m1 === undefined || m2 === undefined) {
-    console.error("Please provide both m1 and m2 values");
-    process.exit(1);
-}
+const m1 = BigInt(args[0] || 25);
+const m2 = BigInt(args[1] || 56);
 
 paillierTest(m1, m2);
