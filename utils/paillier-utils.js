c330e5463bb4b3bf85053e3f3f2ab2fa155ca205
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-12-05 14:10:14 +0530

    chore: improve messaging
    ready to use Cairo code

--- utils/paillier-utils.js
+++ utils/paillier-utils.js
@@ -2,26 +2,18 @@ const paillierBigint = require("paillier-bigint");
 
 async function paillierTest(m1, m2) {
     // (asynchronous) creation of a random private, public key pair for the Paillier cryptosystem
-    const { publicKey, privateKey } = await paillierBigint.generateRandomKeys(
-        64 // key size in bits
-    );
+    const {
+        publicKey,
+        privateKey
+    } = await paillierBigint.generateRandomKeys(keyBitlength);
 
-    console.log(
-        "Public key parameters: \n",
-        "n = ",
-        publicKey.n.toString(),
-        "\n",
-        "g = ",
-        publicKey.g.toString()
-    );
-    console.log(
-        "Private key parameters: \n",
-        "lambda = ",
-        privateKey.lambda.toString(),
-        "\n",
-        "mu = ",
-        privateKey.mu.toString()
-    );
+    console.log(`
+    // Public key params
+    const n: u256 = ${publicKey.n.toString()};
+    const g: u256 = ${publicKey.g.toString()};
+    // Private key params
+    const lambda: u256 = ${privateKey.lambda.toString()};
+    const mu: u256 = ${privateKey.mu.toString()};`);
 
     // Optionally, you can create your public/private keys from known parameters
     // const publicKey = new paillierBigint.PublicKey(n, g)
@@ -34,17 +26,22 @@ async function paillierTest(m1, m2) {
     const c2 = publicKey.encrypt(m2);
     const encryptedSum = publicKey.addition(c1, c2);
 
-    console.log("m1 = ", m1.toString());
-    console.log("m2 = ", m2.toString());
+    console.log(`
+    // Inputs
+    let m1: u256 = ${m1};
+    let m2: u256 = ${m2};
+    // Input ciphers
+    let c1: u256 = ${c1.toString()};
+    let c2: u256 = ${c2.toString()};`);
 
-    console.log("c1 = ", c1.toString());
-    console.log("c2 = ", c2.toString());
-
-    console.log("Encrypted sum = ", encryptedSum.toString());
+    console.log(`
+    // Sum cipher
+    let c_sum = ${encryptedSum.toString()}`);
 }
 
 const args = process.argv.slice(2);
-const m1 = BigInt(args[0] || 25);
-const m2 = BigInt(args[1] || 56);
+const keyBitlength = +args[0] || 64;
+const m1 = BigInt(args[1] || 25);
+const m2 = BigInt(args[2] || 56);
 
 paillierTest(m1, m2);
